70a9c8707aaf1fcb5d73fee23b4a3f97366b2814
MarwahManan <marwahkhan671@gmail.com>
2024-11-13 05:41:03 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/navbar";
 
 export default function Contact() {
-  throw new Error("Contact Page Not Yet Implemented");
+ 
   return (
     <div>
      
       <Navbar/>
       <p className="text-center p-11">Contact us</p>
+      
+            <p>Our contact page will be available soon.</p>
 
     </div>
   )
