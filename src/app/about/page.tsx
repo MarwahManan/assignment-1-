3cd5a490c6a4c690ee8c8dfd4372debc173b9efb
MarwahManan <marwahkhan671@gmail.com>
2024-11-26 17:55:49 +0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
     return (
       <div>
          <Navbar/>
-         <p className="text-center p-11 ">Welcome to M<sup>2</sup> Bookstore , here you will find different categories books with affordable prices!</p>
+         <p className="text-center p-11 ">Welcome to M<sup>2</sup> Bookstore , here you will find different category of  books with affordable prices!</p>
       </div>
     )
   }
